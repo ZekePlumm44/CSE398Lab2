30170fb6da4115af5e59c9ba09a59db8f501ec5c
Isaiah Plummer <iplummer013@gmail.com>
2020-02-05 13:26:07 -0500

    Update rpiTransmitterTest.cpp

--- rpiTransmitterTest.cpp
+++ rpiTransmitterTest.cpp
@@ -1,19 +1,18 @@
-#include <iostream>
+#include <stdio.h>
 #include <wiringPi.h>
 #include <wiringSerial.h> 
 
 using namespace std;
 int pin = 7;
+int fd;
 int main() {
     if((fd = serialOpen("/dev/ttyS0", 9600)) < 0) {
         fprintf(stdout,"Unable to open the thing");
-        exit(1);
+        return 0;
     }
     while(1) {
-        digitalWrite(pin, 0);
-        delay(500);
-        digitalWrite(pin,1);
-        delay(500);
+        serialPutchar(fd,0);
+        serialPutchar(fd,1);
     }
     return 0;
 }
